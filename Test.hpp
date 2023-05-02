5780a170ecbb43f41308f4e6d320f64da5e26c0e
Andree34 <52709475+Andree34@users.noreply.github.com>
2023-05-02 19:26:19 +0200

    Add tests

--- Test.hpp
+++ Test.hpp
@@ -6,6 +6,7 @@
 #include <fstream>
 
 #include "Fast_simplifier.hpp"
+#include "Slow_simplifier.hpp"
 
 template<typename T = Fast_simplifier<>>
 class Test
@@ -18,7 +19,18 @@ public:
 		"SimpleCollinear",
 		"RationalNumbers",
 		"BoundaryBlock",
-		"BoundaryBlock2"
+		"BoundaryBlock2",
+		"RandomTests/0",
+		"RandomTests/1",
+		"RandomTests/2",
+		"RandomTests/3",
+		"RandomTests/4",
+		"RandomTests/5",
+		"RandomTests/6",
+		"RandomTests/7",
+		"RandomTests/8",
+		"RandomTests/9",
+		"RandomTests/10"
 	};
 
 	// run all tests
